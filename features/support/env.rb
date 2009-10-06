e8369c93992889f9cc0201b2cda777ef43b495d3
Nando <fernando.garcia@the-cocktail.com>
2009-10-06 19:09:42 +0200

    Adaptaciones para soportar Cucumber 0.3.102 (preparando un MP multi-lenguaje)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,84 +1,33 @@
 # Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ENV"] ||= "cucumber"
 require File.expand_path(File.dirname(__FILE__) + '/app/config/environment')
 require 'cucumber/rails/world'
-Cucumber::Rails.use_transactional_fixtures
 
-require 'webrat/rails'
+# Whether or not to run each scenario within a database transaction.
+#
+# If you leave this to true, you can turn off traqnsactions on a
+# per-scenario basis, simply tagging it with @no-txn
+Cucumber::Rails::World.use_transactional_fixtures = true
 
-# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
-require 'cucumber/rails/rspec'
-require 'webrat/rspec-rails'
+# Whether or not to allow Rails to rescue errors and render them on
+# an error page. Default is false, which will cause an error to be
+# raised.
+#
+# If you leave this to false, you can turn on Rails rescuing on a
+# per-scenario basis, simply tagging it with @allow-rescue
+ActionController::Base.allow_rescue = false
 
-require 'mundo_pepino'
+# Comment out the next line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode'
+
+require 'webrat'
+require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
 
 Webrat.configure do |config|
   config.mode = :rails
+  config.open_error_files = false # Set to true if you want error pages to pop up in the browser
 end
 
-MundoPepino::ModelsToClean = [
-  Orchard,
-  Terrace,
-  Crop,
-  Fertilizer,
-  Tomato,
-  Chard,
-  Pepino,
-  Sprinkler
-]
+require 'cucumber/rails/rspec'
+require 'webrat/core/matchers'
 
-String.model_mappings = {
-  /^huert[oa]s?$/i            => Orchard,
-  /^bancal(es)?$/i           => Terrace,
-  /^cultivos?$/i             => Crop,
-  /^(abono|fertilizante)s?$/i => Fertilizer,
-  /^tomates?$/i		    => Tomato,
-  /^acelgas?$/i		    => Chard,
-  /^pepinos?$/i		    => Pepino,
-  /^aspersor(es)?$/i        => Sprinkler
-}
-
-String.field_mappings = {
-  /^[Ááa]reas?$/i    => :area,
-  /^color(es)?$/i   => :color,
-  /^latitud(es)?$/i => :latitude,
-  /^longitud(es)?/i => :length,
-  /^pesos?/i        => :weight,
-  /^di[áa]metros?/i => :diameter,
-  /^matas?$/i       => :plants,
-  /^variedad(es)?$/i           => :variety,
-  /^campos? de texto/i         => :text_field,
-  /^[áa]reas? de texto/i       => :textarea,
-  /^tipos? de cultivos?/i      => :orchard_type,
-  /^nacionalidad/i             => :nationality,
-  /^fichero adjunto/i          => :file_field,
-  /^casilla seleccionada/i     => :selected_check,
-  /^casilla sin seleccionar/i  => :unselected_check,
-  /^Orchard::longitud(es)?$/   => :longitude,
-  /^cultivad[oa]$/             => :cultivated,
-  /^caudal(es)?$/i             => :flow,
-  /^unidad(es)? caudal(es)?$/i => :flow_units,
-  /^estaci[oó]n(es)?$/i        => :season
-}
-
-String.url_mappings.merge!(
-  /^la página de registro/i    => '/welcome/signup'
-)
-
-# Si utilizas factory_girl # probado!
-#require 'factory_girl'
-# Definición de las factorias equivalente a example_data.rb en fixture_replacement
-#require File.expand_path(File.dirname(__FILE__) + '/app/db/factories')
-
-class MiMundo < MundoPepino
-  include FixtureReplacement
-  # Helpers específicos de nuestras features
-end
-
-Before do
-  MundoPepino::ModelsToClean.each { |model| model.destroy_all }
-end
-
-World do
-  MiMundo.new
-end
