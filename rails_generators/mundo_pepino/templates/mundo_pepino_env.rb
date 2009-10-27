9543f53305d030c2b2a12b62106dc8a6193b9a05
Nando <fernando.garcia@the-cocktail.com>
2009-10-27 16:22:28 +0100

    Generated require adapted to current file locations

--- rails_generators/mundo_pepino/templates/mundo_pepino_env.rb
+++ rails_generators/mundo_pepino/templates/mundo_pepino_env.rb
@@ -1,16 +1,5 @@
-# Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require 'cucumber/rails/world'
-Cucumber::Rails.use_transactional_fixtures
+require 'mundo_pepino/es_ES'
 
-require 'webrat/rails'
-
-# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
-require 'cucumber/rails/rspec'
-require 'webrat/rspec-rails'
-
-require 'mundo_pepino'
 
 Webrat.configure do |config|
   config.mode = :rails
